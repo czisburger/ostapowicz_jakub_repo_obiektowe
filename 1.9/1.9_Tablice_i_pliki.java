20d009cdc1afaed96a0473c3ddac16f7f4bcc0b8
Jakub <dreon20k@gmail.com>
2021-02-23 10:18:08 +0100

    update 1.9

--- 1.9/1.9_Tablice_i_pliki.java
+++ 1.9/1.9_Tablice_i_pliki.java
@@ -1,8 +1,19 @@
-import java.util.Scanner;
+import java.util.Random;
 
-class JakubOstapowiczTabliceIPliki{
-	public static void main(String[] arg){
+class Tablice{
+    public static void main(String[] a){
+        tablicaInt();
 		
-		}
-	}
+    }
+
+    private static void tablicaInt(){
+        Random r = new Random();
+        int tab1[] = new int[10];
+
+        for(int i=0;i<tab1.length;i++){
+            tab1[i] = r.nextInt(6)+1;
+        }
+		
+		int tab2[] = new int[10];
+    }
 }
