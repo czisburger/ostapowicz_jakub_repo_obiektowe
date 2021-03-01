a476dad6ff488958982112a49c867b0c16c36d39
Jakub <dreon20k@gmail.com>
2021-03-01 09:09:45 +0100

    Stworzenie tablicy jednowymiarowej

--- 1.9/Cwiczenie-01_03_2021.java
+++ 1.9/Cwiczenie-01_03_2021.java
@@ -1,6 +1,10 @@
 
 class TablicaDwuwymiarowa{
     public static void main(String [] args){
-        
+        int [] tab = new int[5];
+        tab[0] = 40;
+        for(int i=0;i<tab.length;i++){
+            System.out.println(tab[i]);
+        }
     }
 }
