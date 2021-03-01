89faf1b04c848e3522a4ae9ffb865a71d4a2a17c
Jakub <dreon20k@gmail.com>
2021-03-01 09:25:28 +0100

    Wyświetlanie tablicy dwuwymiarowej

--- 1.9/Cwiczenie-01_03_2021.java
+++ 1.9/Cwiczenie-01_03_2021.java
@@ -7,5 +7,17 @@ class TablicaDwuwymiarowa{
             System.out.println(tab[i]);
         }
         int [][] tab2d = new int[5][2];
+        tab2d[0][1] = 50;
+        tab2d[2][1] = -200;
+        tab2d[4][0] = 444;
+        System.out.println(tab2d.length);
+        System.out.println(tab2d.length*tab2d[0].length);
+        for(int i=0;i<tab2d.length;i++){
+            for(int j=0;j<tab2d[i].length;j++){
+                System.out.println(tab2d[i][j]);
+            }
+            System.out.println("Koniec pętli ze zmienną j");
+        }
+        System.out.println("Koniec pętli ze zmienną i");
     }
 }
