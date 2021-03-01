6d4634878ad90d386616fade3f282e78032eea57
Jakub <dreon20k@gmail.com>
2021-03-01 09:29:32 +0100

    przeniesienie funkcjonalności do metody tab2dFirst

--- 1.9/Cwiczenie-01_03_2021.java
+++ 1.9/Cwiczenie-01_03_2021.java
@@ -1,17 +1,14 @@
 
 class TablicaDwuwymiarowa{
     public static void main(String [] args){
-        int [] tab = new int[5];
-        tab[0] = 40;
-        for(int i=0;i<tab.length;i++){
-            System.out.println(tab[i]);
-        }
+        
+    }
+
+    private static void tab2dFirst(){
         int [][] tab2d = new int[5][2];
         tab2d[0][1] = 50;
         tab2d[2][1] = -200;
         tab2d[4][0] = 444;
-        System.out.println(tab2d.length);
-        System.out.println(tab2d.length*tab2d[0].length);
         for(int i=0;i<tab2d.length;i++){
             for(int j=0;j<tab2d[i].length;j++){
                 System.out.println(tab2d[i][j]);
