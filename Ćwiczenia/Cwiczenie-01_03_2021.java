73600209a658312598fa4d9f73b61cdb1fff4757
Jakub <dreon20k@gmail.com>
2021-03-02 09:55:29 +0100

    ćwiczenia

--- Ćwiczenia/Cwiczenie-01_03_2021.java
+++ Ćwiczenia/Cwiczenie-01_03_2021.java
@@ -1,6 +1,10 @@
 
 class TablicaDwuwymiarowa{
     public static void main(String [] args){
+        tablica2x2();
+    }
+
+    public static void tablica2x2(){
         
     }
 
---
+++ Ćwiczenia/Cwiczenie-02_03_2021.java
@@ -0,0 +1,8 @@
+class TablicaDwuwymiarowa{
+    public static void main(String [] args){
+        tablica2x2();
+    }
+
+    public static void tablica2x2(){
+        
+    }
