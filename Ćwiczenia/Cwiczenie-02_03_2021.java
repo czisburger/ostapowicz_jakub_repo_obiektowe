10ef0718d8d188542c0d0013f26e98f15f6595e4
Jakub <dreon20k@gmail.com>
2021-03-02 10:33:55 +0100

    Tabliczka mnożenia

--- Ćwiczenia/Cwiczenie-02_03_2021.java
+++ Ćwiczenia/Cwiczenie-02_03_2021.java
@@ -1,17 +1,34 @@
 class TablicaDwuwymiarowa{
     public static void main(String [] args){
-        tablica2x2();
+        tablicaTabliczkaMnozenia();
+    }
+
+    public static void tablicaTabliczkaMnozenia(){
+        int [][] tab = new int[10][10];
+        for(int i=0;i<tab.length;i++){
+            for(int j=0;j<tab[i].length;j++){
+                tab[i][j] = (i+1)*(j+1);
+            }
+        }
+        
+        System.out.println("Tabliczka Mnozenia: ");
+        for(int i=0;i<tab.length;i++){
+            for(int j=0;j<tab[i].length;j++){
+                System.out.print(tab[i][j]+"\t");
+            }
+            System.out.println();
+        }
     }
 
     public static void tablica2x2(){
-        int [][] tab2d = new int[2][2];
-        tab2d[0][0] = 1;
-        tab2d[1][0] = 2;
-        tab2d[0][1] = 3;
-        tab2d[1][1] = 4;
-        for(int i=0;i<tab2d.length;i++){
-            for(int j=0;j<tab2d[i].length;j++){
-                System.out.println(tab2d[i][j]);
+        int [][] tab2x2 = new int[2][2];
+        tab2x2[0][0] = 1;
+        tab2x2[1][0] = 2;
+        tab2x2[0][1] = 3;
+        tab2x2[1][1] = 4;
+        for(int i=0;i<tab2x2.length;i++){
+            for(int j=0;j<tab2x2[i].length;j++){
+                System.out.println(tab2x2[i][j]);
             }
         }
     }
