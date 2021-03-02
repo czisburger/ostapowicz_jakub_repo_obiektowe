261346f16a5777832c9922d63d614673f22d387e
Jakub <dreon20k@gmail.com>
2021-03-02 09:59:47 +0100

    Dodanie kodu do pliku Ćwiczenie-02.03.2021

--- Ćwiczenia/Cwiczenie-02_03_2021.java
+++ Ćwiczenia/Cwiczenie-02_03_2021.java
@@ -4,5 +4,15 @@ class TablicaDwuwymiarowa{
     }
 
     public static void tablica2x2(){
-        
+        int [][] tab2d = new int[2][2];
+        tab2d[0][0] = 1;
+        tab2d[1][0] = 2;
+        tab2d[0][1] = 3;
+        tab2d[1][1] = 4;
+        for(int i=0;i<tab2d.length;i++){
+            for(int j=0;j<tab2d[i].length;j++){
+                System.out.println(tab2d[i][j]);
+            }
+        }
     }
+}
