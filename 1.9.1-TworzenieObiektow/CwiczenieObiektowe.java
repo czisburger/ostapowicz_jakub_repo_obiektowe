aa23f7c1af8a440cc85e019eb50cafad386a876f
Jakub <dreon20k@gmail.com>
2021-03-30 09:56:17 +0200

    zadanie 2

--- 1.9.1-TworzenieObiektow/CwiczenieObiektowe.java
+++ 1.9.1-TworzenieObiektow/CwiczenieObiektowe.java
@@ -2,8 +2,12 @@
 class CwiczenieObiektowe{
     public static void main(String[] args){
         MojeDane md = new MojeDane("Jakub", "Ostapowicz", "2PTN");
+        Logowanie l = new Logowanie("akronim", "123ASDlo9");
+
         System.out.println("Imie: "+md.imie);
         System.out.println("Nazwisko: "+md.nazwisko);
         System.out.println("Klasa: "+md.klasa);
+
+        System.out.println(l.toString());
     }
 }
---
+++ 1.9.1-TworzenieObiektow/Logowanie.java
@@ -0,0 +1,15 @@
+
+class Logowanie{
+    private String login = "";
+    private String haslo = "";
+
+    public Logowanie(String login, String haslo){
+        this.login = login;
+        this.haslo = haslo;
+    }
+
+    @Override
+    public String toString(){
+        return "Login: "+login+" Password:"+haslo;
+    }
+}
