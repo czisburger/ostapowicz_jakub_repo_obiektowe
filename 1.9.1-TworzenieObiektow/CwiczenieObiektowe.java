a5b376c71a1c56cb684d9c7d156f0ca13f4a7e51
Jakub <dreon20k@gmail.com>
2021-03-30 09:36:26 +0200

    zadanie 1

--- 1.9.1-TworzenieObiektow/CwiczenieObiektowe.java
+++ 1.9.1-TworzenieObiektow/CwiczenieObiektowe.java
@@ -1,3 +1,9 @@
+
 class CwiczenieObiektowe{
-    
+    public static void main(String[] args){
+        MojeDane md = new MojeDane("Jakub", "Ostapowicz", "2PTN");
+        System.out.println("Imie: "+md.imie);
+        System.out.println("Nazwisko: "+md.nazwisko);
+        System.out.println("Klasa: "+md.klasa);
+    }
 }
