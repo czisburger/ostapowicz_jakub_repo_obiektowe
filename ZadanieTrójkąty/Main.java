551496dfd3a63e5ae8b4fd3c97cbdc94c55804b7
Jakub <dreon20k@gmail.com>
2021-04-13 09:54:12 +0200

    Trzeci konstruktor

--- ZadanieTrójkąty/Main.java
+++ ZadanieTrójkąty/Main.java
@@ -4,5 +4,6 @@ class Main{
     public static void main(String[] args){
         Triangle t1 = new Triangle(5,4);
         Triangle t2 = new Triangle(6,2,70);
+        Triangle t3 = new Triangle(3,60,50,70);
     }
 }
--- ZadanieTrójkąty/Triangle.java
+++ ZadanieTrójkąty/Triangle.java
@@ -11,4 +11,9 @@ class Triangle{
     public Triangle(int bok1, int bok2, int sin){
         System.out.println("Drugie pole trojkata: "+bok1/2*bok2*sin);
     }
+
+    //Kon - pole trójkąta z jednym bokiem i trzema sinusami
+    public Triangle(int bok1, int sin1, int sin2, int sin3){
+        System.out.println("Trzecie pole trojkata: "+((bok1/2)*(bok1/2))*(sin1*sin2)/sin3);
+    }
 }
