0e0285b63e09da9a645a7430237a45a0086fe675
Jakub <dreon20k@gmail.com>
2021-04-13 09:40:46 +0200

    Pierwszy konstruktor

--- ZadanieTrójkąty/Main.java
+++ ZadanieTrójkąty/Main.java
@@ -2,6 +2,7 @@
 
 class Main{
     public static void main(String[] args){
+        Triangle t1 = new Triangle(5,4);
         
     }
 }
--- ZadanieTrójkąty/Triangle.java
+++ ZadanieTrójkąty/Triangle.java
@@ -1,5 +1,8 @@
 
 
 class Triangle{
-    
+    public Triangle(int wysokosc, int podstawa){
+        //Kon - pole trójkąta z wysokością i podstawą
+        System.out.println("Pole trojkata: "+wysokosc*podstawa/2);
+    }
 }
