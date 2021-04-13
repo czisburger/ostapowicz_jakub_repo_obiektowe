1685ff7c68f07d3ba1866859fd15ef5d710b07c7
Jakub <dreon20k@gmail.com>
2021-04-13 10:07:34 +0200

    Czwarty konstruktor

--- ZadanieTrójkąty/Main.java
+++ ZadanieTrójkąty/Main.java
@@ -3,7 +3,9 @@
 class Main{
     public static void main(String[] args){
         Triangle t1 = new Triangle(5,4);
-        Triangle t2 = new Triangle(6,2,70);
+        Triangle t2 = new Triangle(6,2,40);
         Triangle t3 = new Triangle(3,60,50,70);
+        Triangle t4 = new Triangle(3,9,6,5,0);
+        
     }
 }
--- ZadanieTrójkąty/Triangle.java
+++ ZadanieTrójkąty/Triangle.java
@@ -7,13 +7,20 @@ class Triangle{
         System.out.println("Pierwsze pole trojkata: "+wysokosc*podstawa/2);
     }
 
-    //Kon - pole trójkąta z dwoma bokami i sinusem
+    //Kon - pole trójkąta z dwoma bokami i kątem pomiędzy bokami
     public Triangle(int bok1, int bok2, int sin){
-        System.out.println("Drugie pole trojkata: "+bok1/2*bok2*sin);
+        System.out.println("Drugie pole trojkata: "+(bok1/2)*bok2*sin);
     }
 
     //Kon - pole trójkąta z jednym bokiem i trzema sinusami
     public Triangle(int bok1, int sin1, int sin2, int sin3){
         System.out.println("Trzecie pole trojkata: "+((bok1/2)*(bok1/2))*(sin1*sin2)/sin3);
     }
+
+    //Kon - pole trójkąta z trzema bokami i promieniem
+    public Triangle(int bok1, int bok2, int bok3, int promien,int pusty){
+        System.out.println("Czwarte pole trojkata: "+(bok1*bok2*bok3)/promien);
+    }
+
+    
 }
